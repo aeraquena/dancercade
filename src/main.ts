b0defd280518fe7f6187e8ffb6481e6f3eeb9e1c
Claire Kwong <clairebkwong@gmail.com>
2025-12-10 15:21:23 -0500

    we have pose

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import "./style.css";
 import { PLAYER_1, SYSTEM, on } from "@rcade/plugin-input-classic";
+import { JOINTS, poseLandmarker } from "./pose";
+import { PoseLandmarker, DrawingUtils } from "@mediapipe/tasks-vision";
+import type { NormalizedLandmark } from "@mediapipe/tasks-vision";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 app.innerHTML = `
@@ -13,25 +16,76 @@ const controls = document.querySelector<HTMLDivElement>("#controls")!;
 
 let gameStarted = false;
 
-function update() {
+/* MediaPipe declarations */
+
+const video = document.querySelector<HTMLVideoElement>("#video")!;
+const canvasElement = document.getElementById(
+  "output_canvas"
+) as HTMLCanvasElement;
+const canvasCtx = canvasElement.getContext("2d") as CanvasRenderingContext2D;
+const drawingUtils = new DrawingUtils(canvasCtx as CanvasRenderingContext2D);
+
+// getUsermedia parameters.
+const constraints: MediaStreamConstraints = {
+  video: true,
+};
+
+let lastVideoTime = -1;
+let webcamRunning: Boolean = false;
+
+// Process each video frame and create pose landmarker
+async function predictWebcam() {
+  let startTimeMs = performance.now();
+
+  if (lastVideoTime !== video.currentTime) {
+    lastVideoTime = video.currentTime;
+    poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
+      console.log(result);
+
+      // Drawing tools
+
+      // Drawing utils
+      canvasCtx.save();
+      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+      // Iterates over bodies. We have to extract indices 1-10 from each.
+      for (const landmark of result.landmarks) {
+        //for (const index in result.landmarks) {
+        drawingUtils.drawLandmarks(landmark as NormalizedLandmark[], {
+          radius: (data: any) =>
+            DrawingUtils.lerp((data.from?.z ?? 0) as number, -0.15, 0.1, 5, 1),
+        });
+        drawingUtils.drawConnectors(
+          landmark as NormalizedLandmark[],
+          PoseLandmarker.POSE_CONNECTIONS as any
+        );
+      }
+    });
+  }
+
+  // Call this function again to keep predicting when the browser is ready.
+  if (webcamRunning === true) {
+    window.requestAnimationFrame(predictWebcam);
+  }
+}
+
+function start() {
   if (!gameStarted) {
     if (SYSTEM.ONE_PLAYER) {
       gameStarted = true;
-      status.textContent = "Game Started!";
+    } else if (SYSTEM.TWO_PLAYER) {
+      gameStarted = true;
     }
+
+    requestAnimationFrame(start);
   } else {
-    const inputs: string[] = [];
-    if (PLAYER_1.DPAD.up) inputs.push("↑");
-    if (PLAYER_1.DPAD.down) inputs.push("↓");
-    if (PLAYER_1.DPAD.left) inputs.push("←");
-    if (PLAYER_1.DPAD.right) inputs.push("→");
-    if (PLAYER_1.A) inputs.push("A");
-    if (PLAYER_1.B) inputs.push("B");
-
-    controls.textContent = inputs.length > 0 ? inputs.join(" ") : "-";
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function success(stream) {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predictWebcam);
+        webcamRunning = true;
+      });
   }
-
-  requestAnimationFrame(update);
 }
 
-update();
+start();
